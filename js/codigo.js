a1da2af93eae251509e9e4b21ed3fb9a9785517e
Jesus <jesus.fdz93@gmail.com>
2021-10-18 15:01:37 -0500

    Ejercicio 1 completado

---
+++ js/codigo.js
@@ -0,0 +1,7 @@
+// EJERCICIO 1
+
+// Se muestra un "Hola mundo" en consola. 
+console.log("Hola mundo");
+
+// Se muestra un mensaje por consola que dice "Soy el primer script". 
+console.log("Soy el primer script");
